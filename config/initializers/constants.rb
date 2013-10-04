bd14e4a6410ba8a20cf3b0a7c208d9bbf94c55fe
Alfredo <alfredo.rangel@cesar.org.br>
2013-10-04 00:25:57 -0300

    prod

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
 APP_ID= '373726312761049' # please change!
 APP_SECRET= 'a0a5eb3f41fc1ade77d7dacde06b7069' # please change!
-SITE_URL = 'http://localhost:3000/getfriends' # please change!
+SITE_URL = 'serene-everglades-1924.herokuapp.com/getfriends' # please change!
 
